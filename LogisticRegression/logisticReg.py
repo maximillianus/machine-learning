18fd3ac49b725033c9cba517823f58db9f97eac7
maximillianus <b4nd1c00t@yahoo.com>
2019-01-08 15:13:23 +0700

    add gitignore, logRegr, simple NN

---
+++ LogisticRegression/logisticReg.py
@@ -0,0 +1,20 @@
+"""
+Implementation of logistic regression from scratch in Python
+No external library is to be used
+"""
+
+import csv
+
+print(' ** Start Logistic Regression ** \n')
+
+def prediction():
+    pass
+
+def main():
+    # Do main code here
+    print('hello logistic!')
+    pass
+
+if __name__ == '__main__':
+    main()
+
---
+++ simple_neural_network/nn_keras.py
@@ -0,0 +1,8 @@
+"""
+Simple neural network using keras high level API
+- use tensorflow backend for now
+- maybe try theano backend later on
+"""
+
+print('** Neural Nets on Keras **')
+import keras
